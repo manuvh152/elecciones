24821a2cd3b8e891741e20ac8a9424c01367c7a8
JoseACP <jose.chairez.is@unipolidgo.edu.mx>
2023-07-27 17:19:51 -0600

    apiimplementation

--- app/screens/Predictions.js
+++ app/screens/Predictions.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import BarChartComponent from "../../components/BarChartComponent";
+import axios from "axios";
 
 const Predictions = ({ navigation }) => {
-  const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [
-      {
-        data: [20, 45, 28, 80, 99, 43],
-      },
-    ],
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    // URL de la API
+    const apiUrl = "http://192.168.0.7:5000/api/v1/estado/2";
+
+    // Hacemos la solicitud a la API
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const dataFromApi = response.data;
+        // Formatear los datos para la gráfica
+        const formattedData = formatChartData(dataFromApi);
+        setChartData(formattedData);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos de la API:", error);
+      });
+  }, []);
+
+  // Función para formatear los datos desde la API a un formato adecuado para la gráfica
+  const formatChartData = (dataFromApi) => {
+    const labels = [];
+    const data = [];
+
+    for (const partido in dataFromApi) {
+      labels.push(partido);
+      // Redondear a dos decimales
+      const value = parseFloat(dataFromApi[partido]).toFixed(2);
+      data.push(value);
+    }
+
+    return {
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+        },
+      ],
+    };
   };
 
   return (
@@ -17,7 +51,7 @@ const Predictions = ({ navigation }) => {
       <View style={{ flex: 1, padding: 16 }}>
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
           <Text>C DATA</Text>
-          <BarChartComponent data={data} />
+          {chartData && <BarChartComponent data={chartData} />}
         </View>
       </View>
     </SafeAreaView>

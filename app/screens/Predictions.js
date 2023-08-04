e44d9597dd8fec12436081b8c5004b2be1a5b661
JoseACP <jose.chairez.is@unipolidgo.edu.mx>
2023-08-03 22:39:54 -0600

    graficas

--- app/screens/Predictions.js
+++ app/screens/Predictions.js
@@ -1,88 +1,123 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
-import { Picker } from "@react-native-picker/picker";
-import BarChartComponent from "../../components/BarChartComponent";
-import axios from "axios";
+import { View, Text, SafeAreaView, StyleSheet, Pressable } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import { Picker } from "@react-native-picker/picker";
+import axios from "axios";
+import BarChartComponent from "../../components/BarChartComponent";
 
 const Predictions = ({ navigation }) => {
   const [selectedNumber, setSelectedNumber] = useState(2);
   const [chartData, setChartData] = useState(null);
-  const [partyData, setPartyData] = useState(null);
 
   useEffect(() => {
-    const apiUrl = `http://example-api.com/party_data/${selectedNumber}`;
+    const apiUrl = `http://192.168.0.5:5000/api/v1/estado/${selectedNumber}`;
 
+    // Hacemos la solicitud a la API
     axios
       .get(apiUrl)
       .then((response) => {
-        const partyDataFromApi = response.data;
-        setPartyData(partyDataFromApi);
+        const dataFromApi = response.data;
+        // Formatear los datos para la gráfica
+        const formattedData = formatChartData(dataFromApi);
+        setChartData(formattedData);
       })
       .catch((error) => {
-        console.error("Error al obtener los datos de las partes de la API:", error);
+        console.error("Error al obtener los datos de la API:", error);
       });
   }, [selectedNumber]);
 
+  // Función para formatear los datos desde la API a un formato adecuado para la gráfica
   const formatChartData = (dataFromApi) => {
-    // Función de formateo de datos del gráfico de barras
-    // (esto ya estaba en el código original, puedes mantenerlo aquí)
+    const labels = [];
+    const data = [];
+
+    for (const partido in dataFromApi) {
+      labels.push(partido);
+      // Redondear a dos decimales
+      const value = parseFloat(dataFromApi[partido]).toFixed(2);
+      data.push(value);
+    }
+
+    return {
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+        },
+      ],
+    };
   };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 16 }}>
+      <View style={{ flex: 1, padding: 16, backgroundColor: "black" }}>
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <View className="flex-1 p-[16px] bg-blackapp">
-            <View className="flex-1 p-[16px] mt-4 items-center justify-start">
-              <View className="flex flex-row flex-wrap justify-around content-around h-full w-full">
-                <View className="w-32 h-32 rounded-full border-2 border-purpleapp flex items-center justify-center">
-                  <Text className="text-whiteapp text-3xl">
-                    {partyData?.party1_percentage ?? "Loading..."}
-                  </Text>
-                  <Text className="text-whiteapp">
-                    {partyData?.party1_name ?? "Loading..."}
-                  </Text>
-                </View>
-                <View className="w-32 h-32 rounded-full border-2 border-purpleapp flex items-center justify-center">
-                  <Text className="text-whiteapp text-3xl">
-                    {partyData?.party2_percentage ?? "Loading..."}
-                  </Text>
-                  <Text className="text-whiteapp">
-                    {partyData?.party2_name ?? "Loading..."}
-                  </Text>
-                </View>
-                <View className="w-32 h-32 rounded-full border-2 border-purpleapp flex items-center justify-center">
-                  <Text className="text-whiteapp text-3xl">
-                    {partyData?.party3_percentage ?? "Loading..."}
-                  </Text>
-                  <Text className="text-whiteapp">
-                    {partyData?.party3_name ?? "Loading..."}
-                  </Text>
-                </View>
-                <View className="w-32 h-32 rounded-full border-2 border-purpleapp flex items-center justify-center">
-                  <Text className="text-whiteapp text-3xl">
-                    {partyData?.party4_percentage ?? "Loading..."}
-                  </Text>
-                  <Text className="text-whiteapp">
-                    {partyData?.party4_name ?? "Loading..."}
-                  </Text>
-                </View>
-              </View>
-              <Text className="text-whiteapp pb-4 text-lg">ACCURACY: 200%</Text>
-            </View>
-            <View className="flex-1 p-[16px] mt-4 items-center justify-start">
-              <Text className="text-whiteapp mb-2 text-lg">CHART SHOWING CURRENT TREND</Text>
-              <View className="flex h-[66%] w-full mb-2 bg-[#37373775] rounded border-2 border-pinkapp justify-center items-center">
-                <Text className="text-pinkapp">Test</Text>
-              </View>
-              <Pressable onPress={() => alert('You pressed Share option.')}>
-                <FontAwesome name="share-alt" size={32} color="#ECECEC"/>
-              </Pressable>
-            </View>
+          <Text style={{ color: "white", marginBottom: 2, fontSize: 18 }}>
+            CHART SHOWING CURRENT TREND
+          </Text>
+          <View
+            style={{
+              flex: 1,
+              width: "100%",
+              marginBottom: 2,
+              backgroundColor: "#37373775",
+              borderRadius: 8,
+              borderWidth: 2,
+              borderColor: "#FF1168",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {chartData && <BarChartComponent data={chartData} />}
           </View>
+          <Pressable onPress={() => alert("You pressed Share option.")}>
+            <FontAwesome name="share-alt" size={32} color="#ECECEC" />
+          </Pressable>
         </View>
-        {chartData && <BarChartComponent data={chartData} />}
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "start" }}>
+          <Text style={{ color: "white", paddingBottom: 2, fontSize: 18 }}>
+            USERNAME
+          </Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={selectedNumber}
+            onValueChange={(itemValue, itemIndex) => setSelectedNumber(itemValue)}
+          >
+             <Picker.Item label="Aguascalientes" value={1} />
+            <Picker.Item label="Baja California" value={2} />
+            <Picker.Item label="Baja California Sur" value={3} />
+            <Picker.Item label="Campeche" value={4} />
+            <Picker.Item label="Coahuila" value={5} />
+            <Picker.Item label="Colima" value={6} />
+            <Picker.Item label="Chiapas" value={7} />
+            <Picker.Item label="Chihuahua" value={8} />
+            <Picker.Item label="Distrito Federal" value={9} />
+            <Picker.Item label="Durango" value={10} />
+            <Picker.Item label="Guanajuato" value={11} />
+            <Picker.Item label="Guerrero" value={12} />
+            <Picker.Item label="Hidalgo" value={13} />
+            <Picker.Item label="Jalisco" value={14} />
+            <Picker.Item label="México" value={15} />
+            <Picker.Item label="Michoacán" value={16} />
+            <Picker.Item label="Morelos" value={17} />
+            <Picker.Item label="Nayarit" value={18} />
+            <Picker.Item label="Nuevo León" value={19} />
+            <Picker.Item label="Oaxaca" value={20} />
+            <Picker.Item label="Puebla" value={21} />
+            <Picker.Item label="Querétaro" value={22} />
+            <Picker.Item label="Quintana Roo" value={23} />
+            <Picker.Item label="San Luis Potosí" value={24} />
+            <Picker.Item label="Sinaloa" value={25} />
+            <Picker.Item label="Sonora" value={26} />
+            <Picker.Item label="Tabasco" value={27} />
+            <Picker.Item label="Tamaulipas" value={28} />
+            <Picker.Item label="Tlaxcala" value={29} />
+            <Picker.Item label="Veracruz" value={30} />
+            <Picker.Item label="Yucatán" value={31} />
+            <Picker.Item label="Zacatecas " value={32} />
+            {/* Opciones del Picker */}
+            </Picker>
+        </View>
       </View>
     </SafeAreaView>
   );
